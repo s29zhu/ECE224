c5c79677fe517a82631ec51475f72a808deb0ab5
s29zhu <shashazhu1989@gmail.com>
2013-01-18 16:43:27 -0500

    test files

---
+++ myESystem/software/test/obj/board_diag.d
@@ -0,0 +1,28 @@
+obj/board_diag.o: board_diag.c board_diag.h \
+  ../test_bsp//HAL/inc/alt_types.h ../test_bsp/system.h \
+  ../test_bsp/linker.h ../test_bsp//HAL/inc/sys/alt_irq.h \
+  ../test_bsp//HAL/inc/nios2.h ../test_bsp//HAL/inc/alt_types.h \
+  ../test_bsp/system.h ../test_bsp//drivers/inc/altera_avalon_pio_regs.h \
+  ../test_bsp//HAL/inc/io.h ../test_bsp//HAL/inc/alt_types.h
+
+board_diag.h:
+
+../test_bsp//HAL/inc/alt_types.h:
+
+../test_bsp/system.h:
+
+../test_bsp/linker.h:
+
+../test_bsp//HAL/inc/sys/alt_irq.h:
+
+../test_bsp//HAL/inc/nios2.h:
+
+../test_bsp//HAL/inc/alt_types.h:
+
+../test_bsp/system.h:
+
+../test_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../test_bsp//HAL/inc/io.h:
+
+../test_bsp//HAL/inc/alt_types.h:
